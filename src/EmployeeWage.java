111259bf64a5263719868ac6e1c8c6e7f69c52f9
aravindkarri <aravindkarri98495@gmail.com>
2021-09-06 22:45:03 +0530

    Added: Code to calculate wage using switch case

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,30 +1,35 @@
 public class EmployeeWage
 {
+    //constants
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_RATE_PER_HR = 20;
+
     public static void main(String args[])
     {
-        //constants
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int EMP_RATE_PER_HR = 20;
         //variables
         int dailyWage = 0;
         int empHrs=0;
+        int totalNumOfDays=0;
 
         System.out.println("Welcome to Employee Computation Program on Master Branch");
+
         double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME)
+
+        switch ((int)empCheck)
         {
-            System.out.println("Employee is full time present");
-            empHrs=8;
-        }
-        else if (empCheck == IS_PART_TIME)
-        {
-            System.out.println("Employee is part time present");
-            empHrs=4;
-        }
-        else
-        {
-            System.out.println("Employee is absent");
+
+            case IS_FULL_TIME:
+                System.out.println("Employee is full time present");
+                empHrs=8;
+                break;
+
+            case IS_PART_TIME:
+                System.out.println("Employee is part time present");
+                empHrs=4;
+
+            default:
+                System.out.println("Employee is absent");
         }
         dailyWage=(empHrs*EMP_RATE_PER_HR);
         System.out.println("Employee daily wage is "+dailyWage);
