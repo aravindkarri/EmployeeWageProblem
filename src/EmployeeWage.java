df129817000753de2cd065ffc53efd07c969c7c1
aravindkarri <aravindkarri98495@gmail.com>
2021-09-17 21:34:51 +0530

    Added: calculate company wage for multiple companies

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,23 +1,36 @@
+/**
+ * @author Aravind
+ * class to calculate employee wage for multiple companies.
+ */
 public class EmployeeWage
 {
     //constants
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
-    public static final int EMP_RATE_PER_HR = 20;
-    public static final int NUM_OF_WORKING_DAYS = 20;
-    public static final int MAX_WORKING_HRS = 100;
-
-    public static void calculateEmployeeWage()
+    /**
+     * Method name: calculateEmployeeWage
+     * Description: Each company has its own wage,working hrs and days per month.This method takes company parameters
+     * and can calculate wages for different companies.It follows dry principle and code reuse.
+     * Algorithm : It takes company parameters and calculate the company daily wage till given condition is true
+     * in the while loop. We use switch statement to check employee works for full time or part-time or absent in particular
+     * day.We can get monthly wage of company wage by adding daily wage.
+     * @param companyName
+     * @param empWagePerHour
+     * @param maxWorkingDays
+     * @param maxWorkingHrs
+     */
+    public static void calculateEmployeeWage(String companyName,int empWagePerHour,int maxWorkingDays,int maxWorkingHrs)
     {
         //variables
-        int salary=0;
-        int empHrs=0;
+        int monthlyEmpWage=0;
         int totalWorkingDays=0;
         int totalEmpHrs=0;
         System.out.println("Welcome to Employee Computation Program");
 
-        while (totalWorkingDays<=NUM_OF_WORKING_DAYS && totalEmpHrs<=MAX_WORKING_HRS)
+        while (totalWorkingDays<maxWorkingDays && totalEmpHrs<maxWorkingHrs)
         {
+            int empHrs=0;
+            int dailyEmpWage=0;
             totalWorkingDays++;
             double empCheck = Math.floor(Math.random() * 10) % 3;
             switch ((int)empCheck) {
@@ -31,13 +44,14 @@ public class EmployeeWage
                 default:
                     System.out.println("Employee is absent");
             }
-            totalEmpHrs += empHrs;
+            dailyEmpWage = empWagePerHour *empHrs;
+            totalEmpHrs  += empHrs;
+            monthlyEmpWage += dailyEmpWage;
         }
-        salary = (totalEmpHrs * EMP_RATE_PER_HR);
-        System.out.println("Employee Wage for a month is "+salary);
+        System.out.println("Employee Wage for a month in "+companyName+ " is "+monthlyEmpWage);
     }
-    public static void main(String args[])
-    {
-        calculateEmployeeWage();
+    public static void main(String args[]) {
+        calculateEmployeeWage("Bridgelabz",20,25,100);
+        calculateEmployeeWage("Reliance",20,10,200);
     }
 }
